24dc9ad32edb146cc63ad826c12ecacc5b901ebc
Ling Fung Chan <justinlfchan@gmail.com>
2025-11-10 14:00:30 +0000

    Fixed the little layout issue in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,5 @@
-import { View } from "react-native";
 import EventsScreen from "./events";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <EventsScreen />
-    </View>
-  );
+  return <EventsScreen />;
 }
